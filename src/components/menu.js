2b6f655ae1f131f252ddc313e8e87bcbcc7a96c9
James Minton <james-minton@github.com>
2020-08-03 10:04:03 -0500

    Update home linl

--- src/components/menu.js
+++ src/components/menu.js
@@ -18,7 +18,7 @@ const Menu = ({ activePageId }) => {
           Close
         </div>
         <Col className="dallas-isd" xs={1}>
-          <span className="text">DALLAS ISD</span>
+          {/* <span className="text">DALLAS ISD</span> */}
         </Col>
 
         <Col className="menu-page-names-col" xs={5}>
