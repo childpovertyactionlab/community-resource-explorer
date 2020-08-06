8cfb1a460f98a11c0b96f82fac7f076bfb11f2ac
A Groshek <amy@amygroshek.com>
2020-08-06 14:21:48 -0500

    Update explorer to accept prop

--- src/pages/explorer.js
+++ src/pages/explorer.js
@@ -9,7 +9,11 @@ import { pages } from "../consts"
 
 const ExplorerPage = ({ ...props }) => {
   return (
-    <Layout disableFooter={true} disableHeader={true}>
+    <Layout
+      disableFooter={true}
+      disableHeader={true}
+      activePageId={pages.EXPLORER.id}
+    >
       <SEO title="Explorer" />
       <Explorer toggleMenu={props.toggleMenu} />
     </Layout>
