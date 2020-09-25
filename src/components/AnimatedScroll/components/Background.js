aaccb4a29161ea260ed57f0fa207bd2b7baa2f84
Lane Olson <laneolson@gmail.com>
2020-09-25 16:19:43 -0600

    add xlinkhref for legacy

--- src/components/AnimatedScroll/components/Background.js
+++ src/components/AnimatedScroll/components/Background.js
@@ -4,7 +4,7 @@ import React from "react"
 export const Background = props => {
   return (
     <motion.g {...props}>
-      <image href="/images/home-map-bg.jpg" x="-270" y="-155" width="1232" />
+      <image xlinkHref="/images/home-map-bg.jpg" href="/images/home-map-bg.jpg" x="-270" y="-155" width="1232" />
     </motion.g>
   )
 }
