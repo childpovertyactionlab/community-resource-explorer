d195b5c421da00308234ec7f2234405be53eacba
Lane Olson <laneolson@gmail.com>
2020-09-25 15:51:12 -0600

    include polyfill script

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -7,10 +7,3 @@
 // You can delete this file if you're not using it
 import "./src/styles/style.scss"
 
-// Bring in intersection observer polyfill
-// See https://github.com/gatsbyjs/gatsby/issues/10435#issuecomment-446627549
-export const onClientEntry = async () => {
-  if (typeof IntersectionObserver === `undefined`) {
-    await import(`intersection-observer`);
-  }
-}
